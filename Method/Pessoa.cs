6c78b3f59bb9fbe37a035e3363692b8a24d7e07e
gamer <gamer@PC-FELIPE76>
2024-05-05 19:28:50 -0300

    Adicionar arquivos de projeto.

---
+++ Method/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    internal class Pessoa
+    {
+        //Method 01
+        public void apresentar()
+        {
+            Console.WriteLine("Olá");
+        }
+        //Method 02
+        public void apresentar(string nome)
+        {
+            Console.WriteLine("Olá "+nome);
+        }
+        //Method 03
+        public void apresentar(string nome,int idade)
+        {
+            Console.WriteLine("Olá " + nome + ", você tem " + idade + " Anos.");
+        }
+    }
+}
---
+++ Method/Program.cs
@@ -0,0 +1,6 @@
+﻿using Method;
+
+Pessoa obj = new Pessoa();
+obj.apresentar();
+obj.apresentar("Felipe");
+obj.apresentar("Felipe", 17);
